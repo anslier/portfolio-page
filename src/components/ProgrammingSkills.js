49c07fc6660107f7b95cab8e897bae0fe409adbb
anslier <110771241+anslier@users.noreply.github.com>
2024-10-10 10:38:20 +0800

    Update ProgrammingSkills.js

--- src/components/ProgrammingSkills.js
+++ src/components/ProgrammingSkills.js
@@ -3,260 +3,44 @@ import React from "react";
 function ProgrammingSkills() {
 	return (
 		<div id="programmingSkills">
-			<div class="min-h-screen bg-base-200 pt-12 place-items-center">
-				<h4 class="font-bold pb-2 border-b border-gray-200 text-3xl text-center">
+			<div className="min-h-screen bg-base-200 pt-12 place-items-center">
+				<h4 className="font-bold pb-2 border-b border-gray-200 text-3xl text-center">
 					My Programming Skills
 				</h4>
 
-				<div class="flex justify-center">
-					<ul class="list-disc">
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Python programming language
+				<div className="flex justify-center">
+					<ul className="list-disc">
+						<div className="flex items-center">
+							<li className="px-5 py-10">
+								PHP: Proficient in PHP development, utilizing frameworks like PHPLens and ADODB for
+								database interaction and performance optimization. Developed application for managing
+								financial transactions. Experienced in building and maintaining dynamic websites, web
+								services, APIs using PHP.
 							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
 						</div>
 
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Machine learning and deep learning in Python
-								with scikit-learn and tensorflow
+						<div className="flex items-center">
+							<li className="px-5 py-10">
+								SQL Databases: Experienced in working with SQL databases, including Oracle, MariaDB, and PostgreSQL. Proficient in writing complex queries, optimizing database performance, and designing database schemas. Designed and implemented the database schema for a FinTech application.
 							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
 						</div>
 
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Java programming language
+						<div className="flex items-center">
+							<li className="px-5 py-10">
+								Frontend Development: Proficient in building modern and responsive user interfaces using React, along with styling frameworks like Bootstrap, Tailwind CSS, and Chakra UI.
 							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
 						</div>
 
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Deep learning in Java with Deeplearning4j
-								framework
+						<div className="flex items-center">
+							<li className="px-5 py-10">
+								Java: Experienced in Java development, including backend programming with the Spring Boot framework and deep learning using Deeplearning4j. Familiar with Deeplearning4j for implementing neural networks.
 							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
 						</div>
 
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Backend programming in Java with Spring boot
-								framework
+						<div className="flex items-center">
+							<li className="px-5 py-10">
+								Python: Proficient in Python programming, specializing in machine learning and deep learning using libraries like scikit-learn and TensorFlow.
 							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
-						</div>
-
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								Frontend programming with React framework,
-								Bootstrap, TailwindCSS and ChakraUI
-							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
-						</div>
-
-						<div class="flex items-center">
-							<li class="px-5 py-10">
-								SQL database (MariaDB, PostgreSQL)
-							</li>
-
-							<div class="rating rating-md">
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-								/>
-								<input
-									type="radio"
-									name="rating"
-									class="mask mask-star-2 bg-orange-400"
-									checked
-								/>
-							</div>
 						</div>
 					</ul>
 				</div>
